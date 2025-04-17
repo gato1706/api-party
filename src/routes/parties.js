1c24bb01be6e08625376c80e78ad79671e0b8d10
gato1706 <thiago.gato17g@gmail.com>
2025-04-17 18:55:54 -0300

    criando o getById da party

--- src/routes/parties.js
+++ src/routes/parties.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
-const partyController = require('../controllers/partyController')
+const partyController = require("../controllers/partyController");
 
-router.route("/parties").post((req,res)=> partyController.create(req,res))
+router.route("/parties").post((req, res) => partyController.create(req, res));
+
+router.route("/parties").get((req, res) => partyController.getAll(req, res));
+
+router.route("/parties/:id").get((req, res) => partyController.get(req, res));
 
 module.exports = router;
