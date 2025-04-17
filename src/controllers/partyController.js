4c469b4822053a783c9e8a13389f227b1a61d215
gato1706 <thiago.gato17g@gmail.com>
2025-04-17 18:45:31 -0300

    criar o partyController e a logica de criar festas que contéem um array de serviços

--- src/controllers/partyController.js
+++ src/controllers/partyController.js
@@ -1,8 +1,40 @@
+const { response } = require("express");
 const PartyModel = require("../models/Party");
 
-const partyController = {
+const checkPartyBudget = (budget, services) => {
+  const priceSum = services.reduce((sum, services) => sum + services.price, 0);
 
-    
+  if (priceSum > budget) {
+    return false;
+  }
+
+  return true;
+};
+
+const partyController = {
+  create: async (req, res) => {
+    try {
+      const party = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        budget: req.body.budget,
+        price: req.body.price,
+        image: req.body.image,
+        services: req.body.services,
+      };
+
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu orçamento é insuficiente" });
+      }
+
+      const response = await PartyModel.create(party);
+
+      res.status(201).json({ response });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = partyController;
